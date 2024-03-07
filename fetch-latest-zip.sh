5eb21bffaea8aee67fa20675b1069d81fdc59827
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2024-03-08 02:30:01 +0530

    fetch-latest-zip.sh: Move to github api and fix script

--- fetch-latest-zip.sh
+++ fetch-latest-zip.sh
@@ -8,15 +8,17 @@ else
   cd ~/actions-runner
   echo "Installing Runner!" #Create folder and download zip
   echo "Downloading Zip"
-  version=$(curl -sI https://github.com/actions/runner/releases/latest | grep -o 'location: .*' | head -n 1 | awk -F / '{print $NF}' | cut -d '?' -f 1)
+  response=$(curl -s "https://api.github.com/repos/actions/runner/releases/latest")
+  version=$(echo "$response" | grep -oP '"tag_name": "\K[^"]*')
+  url=$(echo "$response" | grep -oP '"browser_download_url": "\K[^"]*' | grep 'linux-x64.tar.gz')
   
   if [[ -z "${version}" ]]; then
     echo "Failed to retrieve the version number"
     exit 1
   fi # Fetch version
   
-  url=https://github.com/actions/runner/releases/download/v${version}/actions-runner-linux-x64-${version}.tar.gz # Fetch URL
-  
+  url=https://github.com/actions/runner/releases/download/${version}/actions-runner-linux-x64-${version}.tar.gz # Fetch URL
+  url=$(echo "$url" | xargs) # Remove leading/trailing whitespace
   if ! wget -O actions-runner-linux-x64.tar.gz "${url}"; then
     echo "Failed to download the runner package"
   exit 1
