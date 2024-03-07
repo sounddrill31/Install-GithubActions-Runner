07f8178703b997f0aaac87a7799c2526b6c386d8
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2024-03-08 02:37:39 +0530

    Finally fix the script

--- fetch-latest-zip.sh
+++ fetch-latest-zip.sh
@@ -9,7 +9,7 @@ else
   echo "Installing Runner!" #Create folder and download zip
   echo "Downloading Zip"
   response=$(curl -s "https://api.github.com/repos/actions/runner/releases/latest")
-  version=$(echo "$response" | grep -oP '"tag_name": "\K[^"]*')
+  version=$(echo "$response" | grep -oP '"tag_name": v"\K[^"]*'| sed 's/^v//')
   url=$(echo "$response" | grep -oP '"browser_download_url": "\K[^"]*' | grep 'linux-x64.tar.gz')
   
   if [[ -z "${version}" ]]; then
@@ -17,7 +17,7 @@ else
     exit 1
   fi # Fetch version
   
-  url=https://github.com/actions/runner/releases/download/${version}/actions-runner-linux-x64-${version}.tar.gz # Fetch URL
+  url=https://github.com/actions/runner/releases/download/v${version}/actions-runner-linux-x64-${version}.tar.gz # Fetch URL
   url=$(echo "$url" | xargs) # Remove leading/trailing whitespace
   if ! wget -O actions-runner-linux-x64.tar.gz "${url}"; then
     echo "Failed to download the runner package"
