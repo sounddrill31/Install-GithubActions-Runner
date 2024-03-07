c11faa9af3d410aa5c240453facc29f4d9d0e0ef
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2024-03-08 01:51:03 +0530

    Create add-shellrc.sh

---
+++ add-shellrc.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Check if a filename argument was provided
+if [ -z "$1" ]
+then
+   echo "No filename provided. Please provide a shellrc like ~/.bashrc as an argument."
+    exit 1
+fi
+
+# Set the filename variable
+filename=$1
+
+# Check if the alias already exists in the file
+if grep -Fxq "alias gh-runner-install='bash ~/actions-runner/scripts/fetch-latest-zip.sh'" $filename
+then
+    echo "Alias for gh-runner-install already exists in file. Skipping..."
+else
+    # Add the alias for gh-runner-install to the file
+    echo "alias gh-runner-install='bash ~/actions-runner/scripts/fetch-latest-zip.sh'" >> $filename
+    echo "Alias for gh-runner-install added to file."
+fi
+
+# Check if the alias already exists in the file
+if grep -Fxq "alias gh-runner-configure='bash ~/actions-runner/configure.sh'" $filename
+then
+    echo "Alias for gh-runner-configure already exists in file. Skipping..."
+else
+    # Add the alias for gh-runner-configure to the file
+    echo "alias gh-runner-configure='bash ~/actions-runner/configure.sh'" >> $filename
+    echo "Alias for gh-runner-configure added to file."
+fi
+
+# Check if the alias already exists in the file
+if grep -Fxq "alias gh-runner-start='bash ~/actions-runner/run.sh'" $filename
+then
+    echo "Alias for gh-runner-start already exists in file. Skipping..."
+else
+    # Add the alias for gh-runner-start to the file
+    echo "alias gh-runner-start='bash ~/actions-runner/run.sh'" >> $filename
+    echo "Alias for gh-runner-start added to file."
+fi
+
+# Succeed
+exit 0
